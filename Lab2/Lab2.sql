215f7c4125e1d1fc1c0b476f53e667f818d0168a
AnhMarcus <1812725@dlu.edu.vn>
2022-11-29 10:01:04 +0700

    CSDL

---
+++ Lab2/Lab2.sql
@@ -0,0 +1,214 @@
+﻿/* Học phần: Cơ sở dữ liệu
+   Ngày: 22/04/2020
+   Người thực hiện: Nguyễn Khánh Linh
+   MSSV: 1812790	
+   Lớp: CTK42
+*/
+------------------------------------------------
+create database Lab02_QuanlySanXuat
+
+go
+
+use Lab02_QuanlySanXuat
+
+go
+--Tạo bảng tổ sản xuất
+create table ToSanXuat
+(
+MaTSX char(4) primary key, 
+TenTSX nvarchar(10) not null unique 
+)
+go
+--Tạo bảng công nhân
+create table CongNhan
+(
+MACN char(5) primary key,
+Ho nvarchar(20), 
+Ten nvarchar(20), 
+Phai nvarchar(10), 
+NgaySinh date, 
+MaTSX char(4) references ToSanXuat(MaTSX)
+)
+go
+
+--Tạo bảng sản phẩm
+create table SanPham
+(
+MaSP char(5) primary key, 
+TenSP nvarchar(20) not null unique,
+DVT nvarchar(10), 
+TienCong float check(TienCong>0)
+)
+go
+--Tạo bảng Thành phẩm
+create table ThanhPham
+(
+MACN char(5) references CongNhan(MACN) , 
+MaSP char(5) references SanPham(MASP), 
+Ngay date, 
+SoLuong int check (SoLuong >= 0)
+primary key (MACN,MASP)
+)
+--chọn xem các bảng đã tạo
+select * from CongNhan
+select * from SanPham
+select * from ToSanXuat
+select * from ThanhPham
+
+--đưa dữ liệu vào các table
+--Nhập dữ liệu cho table ToSanXuat
+insert into ToSanXuat values('TS01',N'Tổ 1')
+insert into ToSanXuat values('TS02',N'Tổ 2')
+go
+--Nhập dữ liệu cho Congnhan
+set dateformat dmy
+insert into CongNhan values('CN001',N'Nguyễn Trường',N'An',N'Nam','12/05/1981','TS01')
+insert into CongNhan values('CN002',N'Lê Thị Hồng',N'Gấm',N'Nữ','04/06/1980','TS01')   
+insert into CongNhan values('CN003',N'Nguyễn Công',N'Thành',N'Nam','04/05/1981','TS02')  
+insert into CongNhan values('CN004',N'Võ Hữu',N'Hạnh',N'Nam','15/02/1980','TS02')  
+insert into CongNhan values('CN005',N'Lý Thanh',N'Hân',N'Nữ','03/12/1981','TS01') 
+
+go
+
+--Nhập duqx liệu cho sản phẩm
+INSERT INTO SanPham VALUES ('SP001', N'Nồi đất', N'cái', 10000)
+INSERT INTO SanPham VALUES ('SP002', N'Chén', N'cái', 2000)
+INSERT INTO SanPham VALUES ('SP003', N'Bình gốm nhỏ', N'cái', 20000)
+INSERT INTO SanPham VALUES ('SP004', N'Bình gốm lớn', N'cái', 25000)
+
+--Nhập dữ liệu cho ThanhPham
+set dateformat dmy
+insert into ThanhPham values('CN001','SP001','01/02/2007',10)
+insert into ThanhPham values('CN002','SP001','01/02/2007',5)
+insert into ThanhPham values('CN003','SP002','10/01/2007',50)
+insert into ThanhPham values('CN004', 'SP003' ,'12/01/2007', 10)
+insert into ThanhPham values('CN005', 'SP002' ,'12/01/2007' ,100)
+insert into ThanhPham values('CN002' ,'SP004' ,'13/02/2007' ,10)
+insert into ThanhPham values('CN001' ,'SP003', '14/02/2007' ,15)
+insert into ThanhPham values('CN003', 'SP001' ,'15/01/2007' ,20)
+insert into ThanhPham values('CN003' ,'SP004', '14/02/2007' ,15)
+insert into ThanhPham values('CN004', 'SP002', '30/01/2007' ,100)
+insert into ThanhPham values('CN005', 'SP003', '01/02/2007' ,50)
+insert into ThanhPham values('CN001', 'SP001', '20/02/2007', 30)
+
+
+--1) Liệt kê các công nhân theo tổ sản xuất gồm các thông tin: TenTSX, HoTen, NgaySinh, Phai (xếp thứ tự tăng dần của tên tổ sản xuất , Tên của công nhân).
+select ToSanXuat.TenTSX, CongNhan.Ho + ' '+CongNhan.Ten as HoTen, CongNhan.NgaySinh,CongNhan.Phai
+from ToSanXuat,CongNhan
+order by ToSanXuat.TenTSX,CongNhan.Ten
+--2) Liệt kê các thành phẩm mà công nhân ‘Nguyễn Trường An’ đã làm được gồm các thông tin: TenSP, Ngay, SoLuong, ThanhTien (xếp theo thứ tự tăng dần của ngày).
+select SanPham.TenSP, ThanhPham.Ngay, ThanhPham.SoLuong, ThanhPham.SoLuong * SanPham.TienCong as ThanhTien
+from SanPham, ThanhPham,CongNhan
+where CongNhan.MACN=ThanhPham.MACN and 
+	  SanPham.MaSP =ThanhPham.MaSP and 
+	  CongNhan.Ho=N'Nguyễn Trường' and CongNhan.Ten='An'
+	  order by ThanhPham.Ngay 
+--3.1) Liệt kê các nhân viên --(không)-- sản xuất sản phẩm ‘Bình gốm lớn’.
+select *--CongNhan.MACN,Ho,Ten, Phai,NgaySinh,MaTSX/*,SanPham.MaSP,Ngay,SoLuong*/,TenSP,DVT,TienCong
+from CongNhan,ThanhPham ,SanPham
+where CongNhan.MACN =ThanhPham.MACN and
+	  ThanhPham.MaSP = SanPham.MaSP and
+	  not SanPham.TenSP='Bình gốm lớn' 
+
+
+--3.2) Liệt kê các nhân viên sản xuất sản phẩm ‘Bình gốm lớn’.
+select CongNhan.MACN,CongNhan.MaTSX,CongNhan.Ho,CongNhan.Ten,CongNhan.Phai,CongNhan.NgaySinh,SanPham.TenSP
+from CongNhan,SanPham,ThanhPham
+where CongNhan.MACN=ThanhPham.MACN and 
+      ThanhPham.MaSP=SanPham.MaSP and
+	  SanPham.TenSP=N'Bình gốm lớn'
+
+--4) Liệt kê thông tin các công nhân có sản xuất cả ‘Nồi đất’ và ‘Bình gốm nhỏ’.
+select CongNhan.MACN,CongNhan.MaTSX,CongNhan.Ho,CongNhan.Ten,CongNhan.Phai,CongNhan.NgaySinh
+from CongNhan,SanPham,ThanhPham
+where CongNhan.MACN=ThanhPham.MACN and 
+      ThanhPham.MaSP=SanPham.MaSP and
+	  SanPham.TenSP=N'Bình gốm nhỏ' and SanPham.TenSP=N'Nồi đất'
+
+/*select A.MACN,Ho +' '+Ten as HoTen,MaTSX
+from CongNhan A,ThanhPham B,SanPham C
+where A.MACN=B.MACN and B.MaSP=C.MaSP and TenSP=N'Nồi đất'
+		and	 a.MACN in(select D.MACN
+						from ThanhPham D, SanPham E
+						where	 D.MaSP=E.MaSP and	 TenSP=N'Bình gốm nhỏ'*/
+--5) Thống kê Số luợng công nhân theo từng tổ sản xuất.
+select TenTSX, count(CongNhan.MACN) as SoLuongCongNhan --count là hàm đếm tổng số lượng dữ liệu trong các ô
+from CongNhan,ToSanXuat
+where CongNhan.MaTSX = ToSanXuat.MaTSX
+group by ToSanXuat.TenTSX
+
+/*select	A.MaTSX,TenTSX,count(MACN), as SoLuongCN
+from ToSanXuat A,CongNhan B
+where A.MaTSX=B.MaTSX
+group by a.MaTSX,TenTSX*/
+
+--6) Tổng số lượng thành phẩm theo từng loại mà mỗi nhân viên làm được (Ho, Ten, TenSP, TongSLThanhPham, TongThanhTien).
+select Ho ,Ten,TenSP,sum(ThanhPham.SoLuong) as TongSLThanhPham, sum(ThanhPham.SoLuong) * SanPham.TienCong as TongThanhTien 
+from CongNhan,SanPham,ThanhPham  --SUM trả về tổng của một tập hợp
+where SanPham.MaSP = ThanhPham.MaSP and	 CongNhan.MACN = ThanhPham.MACN
+group by CongNhan.MACN,Ho,Ten,TenSP,SanPham.TienCong
+
+--7) Tổng số tiền công đã trả cho công nhân trong tháng 1 năm 2007
+select sum(ThanhPham.SoLuong * SanPham.TienCong) as TongSoTienCong_1_2007
+from CongNhan,SanPham,ThanhPham
+where  SanPham.MaSP = ThanhPham.MaSP and
+	   CongNhan.MACN = ThanhPham.MACN and
+	   month(ThanhPham.Ngay) = 1 and YEAR(Ngay) =2007
+	   
+--8) Cho biết sản phẩm được sản xuất nhiều nhất trong tháng 2/2007
+select
+from ThanhPham A,SanPham B
+where A.MaSP=B.MaSP and	 MONTH(Ngay) =2 and YEAR(Ngay)= 2007
+group by  A.MaSP,TenSP
+having sum(SoLuong)>=all (select SUM(SoLuong)
+							from ThanhPham C
+							where	MONTH(Ngay)=2 and YEAR(ngay)=2007
+							group by C.MaSP)
+--9) Cho biết công nhân sản xuất được nhiều ‘Chén’ nhất.
+select top(1) CongNhan.Ho + ' ' + CongNhan.Ten as HoVaTen
+from CongNhan,SanPham,ThanhPham
+where CongNhan.MACN = ThanhPham.MACN and
+	  ThanhPham.MaSP = SanPham.MaSP and
+	  SanPham.TenSP =N'Chén'
+--10) Tiền công tháng 2/2006 của công nhân viên có mã số ‘CN002’
+select CongNhan.MACN,CongNhan.Ho,CongNhan.Ten,SanPham.TienCong * ThanhPham.SoLuong as TienCong
+from CongNhan,ThanhPham,SanPham
+where CongNhan.MACN = ThanhPham.MACN and
+	  ThanhPham.MaSP = SanPham.MaSP and
+	  month(ThanhPham.Ngay)='2' and CongNhan.MACN='CN002'
+--11) Liệt kê các công nhân có sản xuất từ 3 loại sản phẩm trở lên.
+select	a.MACN,Ho,Ten,COUNT(distinct MaSP) 
+from CongNhan a, ThanhPham b
+where a.MACN=b.MACN
+group by a.MACN,Ho,Ten
+having COUNT(distinct MaSP)>=3
+--12) Cập nhật giá tiền công của các loại bình gốm thêm 1000.
+update SanPham set	TienCong = TienCong + 1000
+where TenSP like  N'Bình gốm%'
+select * from  SanPham
+--13) Thêm bộ <’CN006’, ‘Lê Thị’, ‘Lan’, ‘Nữ’,’TS02’ > vào bảng CongNhan.
+insert into CongNhan(MACN,Ho,Ten,Phai,MaTSX) values ('CN006', N'Lê Thị', N'Lan', N'Nữ','TS02')
+select * from CongNhan
+delete from CongNhan where MACN='CN006'
+
+--III. Thủ tục & Hàm
+--A. Viết các hàm sau:
+--a. Tính tổng số công nhân của một tổ sản xuất cho trước
+create function ufn_TongSoCongNhan(@MACN char(5))
+return int
+As	
+Begin 
+	declare @Tong
+	select @Tong = count(CongNhan.MACN)
+	from CongNhan
+	where MACN = @MACN 
+	
+	end
+--b. Tính tổng sản lượng sản xuất trong một tháng của một loại sản phẩm cho trước.
+--c. Tính tổng tiền công tháng của một công nhân cho trước.
+--d. Tính tổng thu nhập trong năm của một tổ sản xuất cho trước.
+--e. Tính tổng sản lượng sản xuất của một loại sản phẩm trong một khoảng thời gian cho trước.
+--B. Viết các thủ tục sau:
+--a. In danh sách các công nhân của một tổ sản xuất cho trước.
+--b. In bảng chấm công sản xuất trong tháng của một công nhân cho trước
+--(bao gồm Tên sản phẩm, đơn vị tính, số lượng sản xuất trong tháng,  đơn giá, thành tiền).
---
+++ Lab8/Lab8.sql
@@ -0,0 +1,283 @@
+﻿/* Học phần: Cơ sở dữ liệu
+   Người thực hiện: Nguyễn Khánh Linh
+   MSSV: 1812790
+   Lớp: CTK42
+*/
+------------------------------------------------
+Create database Lab08_QuanLySinhVien
+go
+
+use Lab08_QuanLySinhVien
+go
+
+create table KHOA
+(MSKhoa nchar(2) primary key,
+TenKhoa nvarchar(30) not null unique,
+TenTat nchar(4) not null unique);
+
+go
+create table Lop
+(MSLop nchar(4) primary key,
+TenLop nvarchar(30) not null unique,
+MSKhoa nchar(2) references KHOA(MSKhoa),
+NienKhoa nchar(4) not null);
+
+go
+create table Tinh
+(MSTinh nchar(2) primary key,
+TenTinh nvarchar(20) not null unique);
+
+go
+create table MonHoc
+(MSMH nchar(4) primary key,
+TenMH nvarchar(30) not null,
+HeSo tinyint not null);
+
+go
+create table SinhVien
+(MSSV nchar(7) primary key,
+Ho nvarchar(20) not null,
+Ten nchar(10) not null,
+NgaySinh date not null,
+MSTinh nchar(2) references TINH(MSTinh),
+NgayNhapHoc date not null,
+MSLop nchar(4) references LOP(MSLop),
+Phai bit not null,
+DiaChi nvarchar(30) not null,
+DienThoai nchar(10)
+)
+select * from SinhVien
+
+
+go
+create table BANGDIEM
+(MSSV nchar(7) foreign key references SinhVien(MSSV),
+MSMH nchar(4) foreign key references MonHoc(MSMH),
+LanThi tinyint not null,
+Diem float not null,
+constraint PK primary key (MSSV, MSMH, LanThi));
+
+Insert into KHOA Values('1','Cong nghe thong tin','CNTT')
+Insert into KHOA Values('2','Dien tu vien thong','DTVT')
+Insert into KHOA Values('3','Quan tri kinh doanh','QTKD')
+Insert into KHOA Values('4','Cong nghe sinh hoc','CNSH')
+
+Insert into Lop Values('98TH','Tin hoc khoa 1998','1','1998')
+Insert into Lop Values('98VT','Vien thong khoa 1998','2','1998')
+Insert into Lop Values('99QT','Quan tri khoa 1999','3','1999')
+Insert into Lop Values('99TH','Tin hoc khoa 1999','1','1999')
+Insert into Lop Values('99VT','Vien thong khoa 1999','2','1999')
+
+Insert into Tinh Values('01','An Giang')
+Insert into Tinh Values('02','TPHCM')
+Insert into Tinh Values('03','Dong Nai')
+Insert into Tinh Values('04','Long An')
+Insert into Tinh Values('05','Hue')
+Insert into Tinh Values('06','Ca Mau')
+
+Insert into MonHoc Values('QA01','Kinh te vi mo','2')
+Insert into MonHoc Values('QA02','Quan tri chat luong','3')
+Insert into MonHoc Values('TA01','Nhap mon tin hoc','2')
+Insert into MonHoc Values('TA02','Lap trinh co ban','3')
+Insert into MonHoc Values('TB01','Cau truc du lieu','2')
+Insert into MonHoc Values('TB02','Co so du lieu','2')
+Insert into MonHoc Values('VA01','Dien tu co ban','2')
+Insert into MonHoc Values('VA02','Mach so','3')
+Insert into MonHoc Values('VB01','Truyen so lieu','3')
+Insert into MonHoc Values('XA01','Vat ly dai cuong','2')
+
+Insert into SinhVien Values('98TH001','Nguyen Van','An','8/6/1980','01','9/3/1998','98TH','1','12 Tran Hung Dao, Q.1','8234512')
+Insert into SinhVien Values('98TH002','Le Thi','An','10/17/1979','01','9/3/1998','98TH','0','23 CMT8, Q.Tan Binh','0303234342')
+Insert into SinhVien Values('98VT001','Nguyen Duc','Bình','11/25/1981','02','9/3/1998','98VT','1','245 Lac Long Quan, Q.11','8654323')
+Insert into SinhVien Values('98VT002','Tran Ngoc','Anh','8/19/1980','02','9/3/1998','98VT','0','242 Tran Hung Dao, Q.1','')
+Insert into SinhVien Values('99QT001','Nguyen Thi','Oanh','8/19/1973','04','10/5/1999','99QT','0','76 Hung Vuong, Q.5','0901656324')
+Insert into SinhVien Values('99QT002','Le My','Hanh','5/20/1976','04','10/5/1999','99QT','0','12 Pham Ngoc Thach, Q.3','')
+Insert into SinhVien Values('99TH001','Ly Van Hung','Dung','9/27/1981','03','10/5/1999','99TH','1','178 CMT8, Q. Tân Bình','7563213')
+Insert into SinhVien Values('99TH002','Van Minh','Hoang','1/1/1981','04','10/5/1999','99TH','1','272 Ly Thuong Kiet, Q.10','8341234')
+Insert into SinhVien Values('99TH003','Nguyen','Tuan','1/12/1980','03','10/5/1999','99TH','1','162 Tran Hung Dao, Q.5','')
+Insert into SinhVien Values('99TH004','Tran Van','Minh','6/25/1981','04','10/5/1999','99TH','1','147 Dien Bien Phu, Q.3','7236754')
+Insert into SinhVien Values('99TH005','Nguyen Thai','Minh','1/1/1980','04','10/5/1999','99TH','1','345 Le Dai Hanh, Q.11','')
+Insert into SinhVien Values('99VT001','Le Ngoc','Mai','6/21/1982','01','10/5/1999','99VT','0','129 Tran Hung Dao, Q.1','0903124534')
+
+Insert into BANGDIEM Values('98TH001','TA01','1','8.5')
+Insert into BANGDIEM Values('98TH001','TA02','1','8')
+Insert into BANGDIEM Values('98TH001','TB01','1','7')
+Insert into BANGDIEM Values('98TH002','TA01','1','4')
+Insert into BANGDIEM Values('98TH002','TA01','2','5.5')
+Insert into BANGDIEM Values('98TH002','TB01','1','7.5')
+Insert into BANGDIEM Values('98VT001','VA01','1','4')
+Insert into BANGDIEM Values('98VT001','VA01','2','5')
+Insert into BANGDIEM Values('98VT002','VA02','1','7.5')
+Insert into BANGDIEM Values('99QT001','QA01','1','7')
+Insert into BANGDIEM Values('99QT001','QA02','1','6.5')
+Insert into BANGDIEM Values('99QT002','QA01','1','8.5')
+Insert into BANGDIEM Values('99QT002','QA02','1','9')
+Insert into BANGDIEM Values('99TH001','TA01','1','4')
+Insert into BANGDIEM Values('99TH001','TA01','2','6')
+Insert into BANGDIEM Values('99TH001','TB01','1','6.5')
+Insert into BANGDIEM Values('99TH002','TB01','1','10')
+Insert into BANGDIEM Values('99TH002','TB02','1','9')
+Insert into BANGDIEM Values('99TH003','TA02','1','7.5')
+Insert into BANGDIEM Values('99TH003','TB01','1','3')
+Insert into BANGDIEM Values('99TH003','TB01','2','6')
+Insert into BANGDIEM Values('99TH003','TB02','1','8')
+Insert into BANGDIEM Values('99TH004','TB02','1','2')
+Insert into BANGDIEM Values('99TH004','TB02','2','4')
+Insert into BANGDIEM Values('99TH004','TB02','3','3')
+
+select *
+from KHOA
+
+select *
+from Lop
+/*
+select COUNT(A.MSKhoa)
+from Lop A,KHOA B
+where A.MSKhoa=B.MSKhoa
+group by A.MSKhoa*/
+select *
+from Tinh
+
+select *
+from MonHoc
+
+select *
+from SinhVien
+
+select *
+from BANGDIEM
+select MSSV,MSMH,LanThi,Diem
+from BANGDIEM
+group by MSSV,MSMH,LanThi,Diem
+having Diem >=all(select Diem from	 BANGDIEM)
+---------Truy vấn đơn giản
+select *
+from SinhVien A,BANGDIEM B
+where A.MSSV=B.MSSV and b.Diem =(select MAX(Diem)  from BANGDIEM)
+--1) Liệt kê MSSV, Họ, Tên, Địa chỉ của tất cả các sinh viên
+select MSSV,Ho,Ten,DiaChi
+from SinhVien
+--2) Liệt kê MSSV, Họ, Tên, MS Tỉnh của tất cả các sinh viên. Sắp xếp kết quả theo MS
+--tỉnh, trong cùng tỉnh sắp xếp theo họ tên của sinh viên.
+select MSSV,Ho,Ten,MSLop
+from SinhVien
+order by MSTinh,Ho,Ten
+--3) Liệt kê các sinh viên nữ của tỉnh Long An
+select MSSV,Ho,Ten,Tinh.TenTinh,Phai
+from SinhVien ,Tinh
+where  SinhVien.MSTinh=Tinh.MSTinh and SinhVien.MSTinh = '04' and Phai = '0'
+--4) Liệt kê các sinh viên có sinh nhật trong tháng giêng.
+select MSSV,Ho,Ten,DiaChi,NgaySinh
+from SinhVien
+where month(NgaySinh) = '01'
+--5) Liệt kê các sinh viên có sinh nhật nhằm ngày 1/1.
+select MSSV,Ho,Ten,DiaChi,NgaySinh
+from SinhVien
+where month(NgaySinh) = '01' and day(NgaySinh) = '01'
+--6) Liệt kê các sinh viên có số điện thoại.
+select *
+from SinhVien
+where  DienThoai != ''				
+--7) Liệt kê các sinh viên có số điện thoại di động.
+select *
+from SinhVien
+where DienThoai like '0%'	
+--8) Liệt kê các sinh viên tên ‘Minh’ học lớp ’99TH’
+select *
+from SinhVien
+where MSLop = '99TH' and Ten = N'Minh'
+--9) Liệt kê các sinh viên có địa chỉ ở đường ‘Tran Hung Dao’
+select *
+from SinhVien
+where DiaChi like '%Tran Hung Dao%'
+--10) Liệt kê các sinh viên có tên lót chữ ‘Van’ (không liệt kê người họ ‘Van’)
+select *
+from SinhVien
+where Ho like '% Van%'
+--11) Liệt kê MSSV, Họ Ten (ghép họ và tên thành một cột), Tuổi của các sinh viên ở tỉnh
+--Long An.
+select MSSV, Ho +' '+Ten as HoTen, year(GETDATE()) - year(NgaySinh)  as Tuoi
+from SinhVien
+where MSTinh = '04'
+--12) Liệt kê các sinh viên nam từ 23 đến 28 tuổi.
+select MSSV, Ho +' '+Ten as HoTen, year(GETDATE()) - year(NgaySinh)  as Tuoi,Phai
+from SinhVien
+where Phai = '1' and year(GETDATE()) - year(NgaySinh) between 23 and 28
+--13) Liệt kê các sinh viên nam từ 32 tuổi trở lên và các sinh viên nữ từ 27 tuổi trở lên.
+select MSSV, Ho +' '+Ten as HoTen, year(GETDATE()) - year(NgaySinh)  as Tuoi,Phai
+from SinhVien
+where Phai = '1' and year(GETDATE()) - year(NgaySinh) >=32
+or	Phai = '0' and year(GETDATE()) - year(NgaySinh) >=27
+
+--14) Liệt kê các sinh viên khi nhập học còn dưới 18 tuổi, hoặc đã trên 25 tuổi.
+select MSSV, Ho +' '+Ten as HoTen, year(GETDATE()) - year(NgaySinh)  as Tuoi,Phai
+from SinhVien
+where  year(NgayNhapHoc) - year(NgaySinh) <=18
+or year(GETDATE()) - year(NgaySinh) >=25
+--15) Liệt kê danh sách các sinh viên của khóa 99 (MSSV có 2 ký tự đầu là ‘99’).
+select *
+from SinhVien
+where MSSV like '99%'
+--16) Liệt kê MSSV, Điểm thi lần 1 môn ‘Co so du lieu’ của lớp ’99TH’
+select A.MSSV,B.LanThi,B.Diem,B.MSMH
+from SinhVien A, BANGDIEM B
+where A.MSSV = B.MSSV and
+	B.MSMH = 'TB02' and A.MSLop like '99%' and b.LanThi = '1'
+		
+--17) Liệt kê MSSV, Họ tên của các sinh viên lớp ’99TH’ thi không đạt lần 1 môn ‘Co so
+--du lieu’
+select A.MSSV,B.LanThi,B.Diem,B.MSMH
+from SinhVien A, BANGDIEM B
+where A.MSSV = B.MSSV and
+	B.MSMH = 'TB02' and A.MSLop like '99%' and b.LanThi = '1' and B.Diem < 4
+
+--18) Liệt kê tất cả các điểm thi của sinh viên có mã số ’99TH001’ theo mẫu sau:
+--MSMH TênMH Lầnthi Điểm
+select B.MSMH,c.TenMH,B.LanThi,B.Diem
+from SinhVien A,BANGDIEM B,MonHoc C
+where a.MSSV = b.MSSV and B.MSMH = C.MSMH and a.MSSV = '99TH001'
+--19) Liệt kê MSSV, họ tên, MSLop của các sinh viên có điểm thi lần 1 môn ‘Co so du
+--lieu’ từ 8 điểm trở lên
+select A.MSSV,A.Ho,A.Ten,A.MSLop
+from SinhVien A, BANGDIEM B
+where A.MSSV = B.MSSV and
+	b.LanThi = '1' and b.Diem >=8
+--20) Liệt kê các tỉnh không có sinh viên theo học
+	select *
+	from Tinh
+	where MSTinh not in(select distinct MSTinh
+						from SinhVien
+						)
+--21) Liệt kê các sinh viên hiện chưa có điểm môn thi nào.
+select *
+from SinhVien
+where MSSV not in(select MSSV
+				  from BANGDIEM
+				  )
+
+--Truy vấn gom nhóm
+--22) Thống kê số lượng sinh viên ở mỗi lớp theo mẫu sau: MSLop, TenLop, SoLuongSV
+select A.MSLop,B.TenLop,COUNT(MSSV)	AS SoLuong
+from SinhVien A,Lop B
+where A.MSLop = B.MSLop
+group by A.MSLop,B.TenLop
+select *from SinhVien
+--23) Thống kê số lượng sinh viên ở mỗi tỉnh theo mẫu sau:
+--MSTinh TênTỉnh SốSVNam SốSVNữ Tổngcộng
+select A.MSTinh, A.TenTinh, count(case B.Phai when '1' then 1 else null end) as SoVSNam, count(case B.Phai when '0' then 1 else null end) as SoVSNu,COUNT(*) as SoLuong
+from Tinh A, SinhVien B
+where A.MSTinh =B.MSTinh
+group by A.MSTinh,A.TenTinh
+
+--24) Thống kê kết quả thi lần 1 môn ‘Co so du lieu’ ở các lớp, theo mẫu sau
+select A.MSLop,A.TenLop,COUNT(case when B.Diem >='4' then 1 else null end) AS N'Số SV đạt',
+						COUNT(case when B.Diem >='4' then 1 else null end)*100/COUNT(*) as N'Tỉ lệ đạt (%)',
+						COUNT(case when B.Diem <'4' then 1 else null end) AS N'Số SV không đạt'
+						,COUNT(case when B.Diem <'4' then 1 else null end)*100/COUNT(*) as N'Tỉ lệ không đạt'
+
+from Lop A, BANGDIEM B, SinhVien C
+where A.MSLop = C.MSLop and C.MSSV = B.MSSV
+group by A.MSLop,A.TenLop
+
+--25) Lọc ra điểm cao nhất trong các lần thi cho các sinh viên theo mẫu sau (điểm in ra của
+--mỗi môn là điểm cao nhất trong các lần thi của môn đó):
